bf6b6c2724a3da608e6b24363cba73df27ca44ca
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2022-04-26 10:46:49 +0200

    Doing dashboard

---
+++ src/viewers/Dashboard.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import '../App/App.css';
+import SideMenu from "../components/SideMenu";
+import { CssBaseline, ThemeProvider, Box } from '@mui/material';
+import Header from "../components/Header";import UI_liste_batterie from '../containers/UI_liste_batterie';
+import {makeStyles} from '@mui/styles'
+import { useState } from "react";
+import Progress from '../components/Progress';
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true
+    }
+  }
+})
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '250px',
+    width: '100%'
+  },
+})
+
+function ListBatterie(){
+    const classes = useStyles();
+    const [process, setProcess] = useState(true);
+
+    setTimeout(() => {
+      setProcess(false);
+    }, [500]);
+    return (
+      < Box >
+      {
+        process ? (
+          <></>
+        ) : (
+          <>
+            <ThemeProvider theme={theme}>
+              <SideMenu />
+              <div className={classes.appMain}>
+                <Header />
+                
+                <UI_liste_batterie />
+              </div>
+              <CssBaseline />
+            </ThemeProvider>
+          </>
+        )
+      }
+    </Box >
+    )
+}
+export default ListBatterie
